549cab82af29b0d0793cd4ae2a01b6b08f17741e
antares <antares_lin@manka.co.jp>
2024-10-02 11:28:57 +0900

    optimize producer

--- spring-cloud-ali-common/src/main/java/spring/cloud/ali/common/component/RocketMQProducer.java
+++ spring-cloud-ali-common/src/main/java/spring/cloud/ali/common/component/RocketMQProducer.java
@@ -1,17 +1,35 @@
 package spring.cloud.ali.common.component;
 
+import com.google.common.base.Throwables;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.rocketmq.spring.core.RocketMQTemplate;
-import org.springframework.messaging.support.MessageBuilder;
+import org.apache.rocketmq.client.exception.MQClientException;
+import org.apache.rocketmq.client.producer.DefaultMQProducer;
+import org.apache.rocketmq.client.producer.SendResult;
+import org.apache.rocketmq.common.message.Message;
+import org.springframework.beans.factory.annotation.Value;
 import spring.cloud.ali.common.util.JsonUtil;
 
-import javax.annotation.Resource;
+import javax.annotation.PostConstruct;
+import java.nio.charset.StandardCharsets;
 
 @Slf4j
 public class RocketMQProducer {
 
-    @Resource
-    private RocketMQTemplate template;
+    @Value("${spring.rocketmq.name-server}")
+    private String nameServer;
+
+    @Value("${spring.rocketmq.producer.group}")
+    private String producerGroup;
+
+    private DefaultMQProducer delegate;
+
+    @PostConstruct
+    public void onInit() throws MQClientException {
+        DefaultMQProducer producer = new DefaultMQProducer(producerGroup);
+        producer.setNamesrvAddr(nameServer);
+        producer.start();
+        delegate = producer;
+    }
 
     /**
      * 发送MQ消息
@@ -20,10 +38,22 @@ public class RocketMQProducer {
      * @param message   消息内容
      */
     public boolean send(String topic, String key, Object message) {
-        template.send(topic, MessageBuilder.withPayload(JsonUtil.toJson(message))
-                .setHeader("KEYS", key)
-                .build());
-        log.info("message produce: topic={}, key={}, message={}", topic, key, message);
+
+        Message msg = new Message();
+        msg.setTopic(topic);
+        msg.setKeys(key);
+        msg.setBody(JsonUtil.toJson(message).getBytes(StandardCharsets.UTF_8));
+
+        try {
+            SendResult res = delegate.send(msg);
+            log.info("message send: topic={}, key={}, message={}, result={}", topic, key, message, res);
+            // TODO 发送可靠性保证
+        } catch (Throwable e) {
+            log.error("message send failed: topic={}, key={}, message={}, error={}",
+                    topic, key, message, Throwables.getStackTraceAsString(e));
+            return false;
+        }
+
         return true;
     }
 }
--- spring-cloud-ali-common/src/main/java/spring/cloud/ali/common/config/RocketMQConfig.java
+++ spring-cloud-ali-common/src/main/java/spring/cloud/ali/common/config/RocketMQConfig.java
@@ -1,31 +1,12 @@
 package spring.cloud.ali.common.config;
 
-import org.apache.rocketmq.client.producer.DefaultMQProducer;
-import org.apache.rocketmq.spring.core.RocketMQTemplate;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import spring.cloud.ali.common.component.RocketMQProducer;
 
 public class RocketMQConfig {
 
-    @Value("${spring.rocketmq.name-server}")
-    private String nameServer;
-
-    @Value("${spring.rocketmq.producer.group}")
-    private String producerGroup;
-
     @Bean
     public RocketMQProducer rocketMQProducer(){
         return new RocketMQProducer();
-    }
-
-    @Bean
-    public RocketMQTemplate rocketTemplate(){
-        DefaultMQProducer producer = new DefaultMQProducer(producerGroup);
-        producer.setNamesrvAddr(nameServer);
-
-        RocketMQTemplate template = new RocketMQTemplate();
-        template.setProducer(producer);
-        return template;
     }
 }
